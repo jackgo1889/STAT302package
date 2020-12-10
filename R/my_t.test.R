f88c4eae330b779d0309695d0c54ea3b87390d01
Jack Go <jackgo1998@gmail.com>
2020-12-09 21:28:17 -0800

    created my_t.test

---
+++ R/my_t.test.R
@@ -0,0 +1,55 @@
+#' T Test Function
+#'
+#' This function performs a one sample t-test.
+#'
+#' @param x Numeric vector of data.
+#' @param alternative Character string specifying the alternative hypothesis.
+#'  only accepts strings \code{"two.sided}, \code{"less"}, or \code{"greater"}.
+#' @param mu Numeric representing the null hypothesis value of the mean.
+#' @keywords inference
+#'
+#' @return List with elements
+#'   \code{test_stat}: the numeric test statistic,
+#'   \code{df}: the degrees of freedom,
+#'   \code{alternative}: the value of the parameter \code{alternative},
+#'   \code{p_val}: the numeric p-value.
+#'
+#' @examples
+#' \dontrun {
+#' data_1 <- c(1:25)
+#' my_t.test(data_1, "two.sided", 0)
+#'
+#' data_2 <- c(1:100)
+#' my_t.test(data_2, "less", 49)
+#' }
+#'
+#' @export
+my_t.test <- function(x, alternative, mu) {
+  #defining degrees of freedom
+  df <- length(x) - 1
+  #defining test statistic
+  test_stat <- (mean(x) - mu) / (sd(x) / sqrt(length(x)))
+  #defining p value
+  p_val = 0
+  #creating a list from defined elements
+  my_list <- list("test statistic" = test_stat,
+                  "degrees of freedom" = df,
+                  "alternative hypothesis" = alternative,
+                  "p_value" = p_val)
+  #if doing a two sided test, this runs
+  if (alternative == "two.sided") {
+    my_list$p_value <- 2*pt(abs(test_stat), df, lower.tail = FALSE)
+    return(my_list)
+    #if alternative test is less, this runs
+  } else if (alternative == "less") {
+    my_list$p_value <- pt(test_stat, df, lower.tail = TRUE)
+    return(my_list)
+    #if alternative test is greater, this runs
+  } else if (alternative == "greater") {
+    my_list$p_value <- pt(test_stat, df, lower.tail = FALSE)
+    return(my_list)
+    #if alternative value is none of the above specified strings, this error message will pop up
+  } else {
+    stop("Alternative value must be 'two.sided', 'less', or 'greater'.")
+  }
+}
